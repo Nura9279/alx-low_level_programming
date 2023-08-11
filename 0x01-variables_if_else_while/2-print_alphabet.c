61aafd9df91f0974fccfe3b59349968a466ee954
Nura <moonlight9279@gmail.com>
2023-08-11 01:00:31 -0700

    2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,22 @@
+
+/**
+ * main - entry point
+ *
+ * Description: print all alphabet letters
+ *
+ * Return: always 0
+*/
+
+int main(void)
+{
+	char ch = 'a';
+
+	while (ch <= 'z')
+	{
+		putchar (ch);
+		ch++;
+	}
+	putchar('\n');
+
+	return (0);
+}
